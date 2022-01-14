20ff717cebc56094a93720d24c55a5443ded7560
lotusfish <Unown256@gmail.com>
2022-01-14 17:35:25 +0000

    launch update

---
+++ scripts/inane/audioPlayer.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+func _process(delta):
+	if get_tree().get_current_scene().get_name() == "Arena":
+		if $battle.playing == false:
+			$menu.playing = false
+			$battle.playing = true
+	else:
+		if $menu.playing == false:
+			$battle.playing = false
+			$menu.playing = true
