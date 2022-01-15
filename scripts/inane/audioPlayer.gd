57e1e0469311bcb422cecc12b957abf0b6f51207
lotusfish <Unown256@gmail.com>
2022-01-15 16:41:51 +0000

    game now picks one of two random fight themes

--- scripts/inane/audioPlayer.gd
+++ scripts/inane/audioPlayer.gd
@@ -1,10 +1,22 @@
 extends Node2D
 
+var music = [
+	[
+		preload("res://sound/music/menuTheme.mp3")
+	], #menu themes
+	[
+		preload("res://sound/music/battleTheme.mp3"),
+		preload("res://sound/music/battle2.mp3")
+	] #battle themes
+]
 
+func _ready():
+	randomize()
 
 func _process(delta):
 	if get_tree().get_current_scene().get_name() == "Arena":
 		if $battle.playing == false:
+			$battle.stream = music[1][randi() % music[1].size()]
 			$menu.playing = false
 			$battle.playing = true
 	else:
